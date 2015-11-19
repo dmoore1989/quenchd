d5ddcc6b183b37d87aabf779a0342c2ed446bb6c
dmoore1989 <douglas.moore89@gmail.com>
2015-11-19 14:16:47 -0500

    delete listeners from comments component, information will filter down from check ins

--- app/assets/javascripts/components/comment/comments_index.js.jsx
+++ app/assets/javascripts/components/comment/comments_index.js.jsx
@@ -1,17 +1,5 @@
 window.CommentsIndex = React.createClass({
 
-  componentDidMount: function () {
-    CommentStore.addChangeHandler(this.updateComments);
-  },
-
-  componentWillUnmount: function () {
-    CommentStore.removeChangeHandler(this.updateComments);
-  },
-
-  updateComments: function () {
-    this.setState({comments: CommentStore.all()});
-  },
-
   render: function () {
     if (this.props.comments){
       return (

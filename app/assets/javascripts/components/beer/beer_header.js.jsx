7e6bda47e742dc98fbee63b2845fce7fe4cfbe3f
dmoore1989 <douglas.moore89@gmail.com>
2015-11-20 14:16:14 -0500

    FInish beer header

--- app/assets/javascripts/components/beer/beer_header.js.jsx
+++ app/assets/javascripts/components/beer/beer_header.js.jsx
@@ -1,5 +1,32 @@
 window.BeerHeader = React.createClass({
+  getInitialState: function () {
+    return ({
+      checkInForm: "hidden"
+    });
+  },
+
+  renderCheckInModal: function () {
+    var klass = ((this.state.checkInForm === "hidden") ? "visible" : "hidden");
+    this.setState({checkInForm: klass});
+  },
+
   render: function () {
-    return (<div>IT IS A BEER</div>);
+    return (
+      <div>
+        <h1>{this.props.beer.name}</h1>
+        <h3>{this.props.beer.style}</h3>
+        <colgroup>
+          <col>{this.props.beer.abv}</col>
+          <col>{this.props.beer.ibu}</col>
+        </colgroup>
+        <button onClick={this.renderCheckInModal}>
+            Check In to This Beer
+        </button>
+        <div className={this.state.checkInForm} >
+          <CheckInForm
+            beer={this.props.beer} />
+        </div>
+      </div>
+    );
   }
 });

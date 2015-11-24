0fd0d20627600d8624dc7f1b891981252a5c8af8
dmoore1989 <douglas.moore89@gmail.com>
2015-11-24 08:57:39 -0500

    Add check in instructions for beer

--- app/assets/javascripts/components/index.js.jsx
+++ app/assets/javascripts/components/index.js.jsx
@@ -4,7 +4,7 @@ Index = React.createClass({
   render: function (){
     return (
       <div className="content">
-        <h5 className="page-title">Recent Activity</h5>
+        <h5 className="page-title">Recent Activity- Click into a Beer to Check In</h5>
         <CheckInIndex checkInData={QuenchdConstants.FETCH} />
         <ReactRouter.Link to="/beers/new">Create a New Beer</ReactRouter.Link>
       </div>

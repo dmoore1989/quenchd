989d9b42164995602934fea022ea7e02dd7b920b
dmoore1989 <douglas.moore89@gmail.com>
2015-11-26 12:31:24 -0500

    Venue pagination

--- app/assets/javascripts/util/venue_api_util.js
+++ app/assets/javascripts/util/venue_api_util.js
@@ -1,12 +1,15 @@
 window.VenueApiUtil = {
-  fetchVenue: function (id) {
+  fetchVenue: function (id, pageNumber) {
     $.ajax({
       url: "/api/venues/" + id,
       type: "GET",
       dataType: "json",
+      data: {page_number: pageNumber}
       success: (function (data) {
-        CheckInApiAction.clearCheckIns();
-        VenueApiAction.receiveVenue(data.venue, data.likes);
+        if (pageNumber === 1) {
+          CheckInApiAction.clearCheckIns();
+          VenueApiAction.receiveVenue(data.venue, data.likes);
+        }
         CheckInApiAction.receiveCheckIns(data.check_ins);
       })
     });

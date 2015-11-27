86f47edfbc25e379445dac0ac5e73cc35691167c
dmoore1989 <douglas.moore89@gmail.com>
2015-11-27 14:01:55 -0500

    Finish seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,11 +52,15 @@ monks.beers.create(name: "Monniken 10", style: "Quad", abv: 8.9 , ibu: 55, image
 belgium.beers.create(name: "Flat Tire", style: "Belgian Amber", abv: 6.5 , ibu: 55, image: "http://images.thecarconnection.com/med/flat-tire-by-flickr-user-tiger-girl_100398893_m.jpg")
 belgium.beers.create(name: "Cranberry Pumpkin Spice White Lambic Ale", style: "Wild Ale", abv: 7.2 , ibu: 15, image: "http://3.bp.blogspot.com/-8CoE2F0L7Gg/VKf_4jhle1I/AAAAAAAAGiA/ET7YGAd0jFI/s1600/lambic2.jpg")
 belgium.beers.create(name: "Ohh my Gueuze!!!", style: "Gueze", abv: 6.5 , ibu: 55, image: "http://cdn.psfk.com/wp-content/uploads/2012/06/ampleforth-abbey-monk-beer.jpg")
-belgium.beers.create(name: "Audacity of Hops", style: "Belgian IPA" abv: 7.8, ibu: 78, image: "https://beeractivist.files.wordpress.com/2008/10/audacity-of-hops.jpg?w=450&h=590")
+belgium.beers.create(name: "Audacity of Hops", style: "Belgian IPA", abv: 7.8, ibu: 78, image: "https://beeractivist.files.wordpress.com/2008/10/audacity-of-hops.jpg?w=450&h=590")
 
 john.beers.create(name: "Worcester Lager", style: "German Lager", abv: 5.5 , ibu: 55, image: "http://ransfordpc.com/pestcontrolblog/wordpress/wp-content/uploads/2014/03/map_of_worcester_ma.jpg")
 john.beers.create(name: "Really Bad Tasting Cherry Beer", style: "Fruit Ale", abv: 5.0 , ibu: 15, image: "http://3.bp.blogspot.com/-8CoE2F0L7Gg/VKf_4jhle1I/AAAAAAAAGiA/ET7YGAd0jFI/s1600/lambic2.jpg")
 john.beers.create(name: "Ohh my Gourd!!! Pumpkin Ale", style: "Gueze", abv: 6.5 , ibu: 45, image: "http://drinks.seriouseats.com/images/2011/09/20110930_173049_Homebrew_Pumpkin.jpg")
-john.beers.create(name: "Octoberfest", style: "Marzen" abv: 6.8, ibu: 57, image: "http://40.media.tumblr.com/37eea6a15d2779dbfef73f666ded14fb/tumblr_mueoanGAvH1siovoto1_500.jpg")
+john.beers.create(name: "Octoberfest", style: "Marzen", abv: 6.8, ibu: 57, image: "http://40.media.tumblr.com/37eea6a15d2779dbfef73f666ded14fb/tumblr_mueoanGAvH1siovoto1_500.jpg")
 
 User.create(username: "DougMoore", email: "doug@email.com", password: "hello1", location: "NYC", image: "http://www.douglasmoore.me/assets/douglas-moore-079fb856e207c7332a5244c1f28a5a6fb0d0b453e0dfeedb329879212c6de367.jpg")
+User.create(username: "DUFFMAN", email: "duffman@ohyeah.com", password: "hello1", location: "NYC", image: "http://vignette2.wikia.nocookie.net/simpsons/images/c/ce/Duff_man.png/revision/latest?cb=20130808075436")
+User.create(username: "Hank Hill", email: "hank@stricklandpropene.com", password: "hello1", location: "Arlen", image: "http://vignette2.wikia.nocookie.net/legendsofthemultiuniverse/images/7/7c/08-King-of-Hill-Hank_l.jpg/revision/latest?cb=20140902033620")
+User.create(username: "Red Ranger", email: "doug@email.com", password: "hello1", location: "NYC", image: "http://pre05.deviantart.net/0662/th/pre/i/2013/289/0/3/red_ranger_by_thuddleston-d6qq8jy.jpg")
+User.create(username: "guest", email: "guest@guest.com", password: "hello1", location: "NYC")

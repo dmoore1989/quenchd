3436c4fd8758c42909e60600a0aa11fd288318e0
dmoore1989 <douglas.moore89@gmail.com>
2015-11-27 05:27:54 -0500

    Begin styling sidebar items

--- app/assets/javascripts/components/applets/user_snapshot.js.jsx
+++ app/assets/javascripts/components/applets/user_snapshot.js.jsx
@@ -2,10 +2,15 @@ window.UserSnapshot = React.createClass({
 
   render: function () {
     return (
-      <div>
-        <div>{this.props.user.username}</div>
-        <div>{this.props.user.count}</div>
-        <div>{this.props.user.uniqueCount}</div>
+      <div className="sidebar-applet">
+        <div className="sidebar-applet-header">
+          <div><h4>{this.props.user.username}</h4></div>
+          <div><h6>{this.props.user.email}</h6></div>
+          <div><h6>{this.props.user.location}</h6></div>
+        </div>
+          <div>{this.props.user.count}</div>
+          <div>{this.props.user.uniqueCount}</div>
+          
       </div>
     );
   }
